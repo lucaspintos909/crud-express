c9b2886f49e66be2022a30b372558f3e36115ec5
Lucas Pintos <56004558+lucaspintos909@users.noreply.github.com>
2020-10-20 11:38:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,11 +11,9 @@ const app = express();
 //Conexion a la bd
 const mongoose = require('mongoose');
 
-/* const uri = 'mongodb://localhost:27017/crud-mevn'; */
+const uri = 'mongodb://localhost:27017/crud-mevn';
 //conexxion en la nube
 
-
-const uri = 'mongodb+srv://lucaspintos909:bicyclestandar@clusterprueba.7nsj3.gcp.mongodb.net/notas-crud?retryWrites=true&w=majority';
 
 const options = {useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true};
 
